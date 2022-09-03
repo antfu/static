2d42c23f74c979e98c843257e2470bf5272e17c3
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-03 13:54:28 +0800

    chore: update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 npx sponsorkit --dir=. -w=800
 npx sponsorkit --dir=. -w=1800 --name=sponsors.wide
-npx sponsorkit --dir=. -w=800 --name=sponsors.part1 --filter=">50"
-npx sponsorkit --dir=. -w=800 --name=sponsors.part2 --filter="<=50"
+npx sponsorkit --dir=. -w=800 --name=sponsors.part1 --filter=">=50"
+npx sponsorkit --dir=. -w=800 --name=sponsors.part2 --filter="<50"
